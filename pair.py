9c5ac2f91bbdaaa1e79514b3ff9cb8f90f8d8328
Usbeje <lhi159712@gmail.com>
2025-03-22 06:41:24 +0700

    Update pair.py

--- pair.py
+++ pair.py
@@ -4,7 +4,7 @@ import requests
 indodax_address = "0x[D17479ad4946665379F19C67572af384A0C2a883]"
 
 # The Etherscan API key
-api_key = "YOUR_API_KEY"
+api_key = "PJM2GWCZVCKGD6R61Y2H4GDS548JVF67WK"
 
 # The Etherscan API endpoint for resolving ENS names
 url = f"https://api.etherscan.io/api?module=account&action=txlist&address={indodax_address}&startblock=0&endblock=99999999&sort=asc&apikey={api_key}"
